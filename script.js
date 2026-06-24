1c59976c25ea57d60e9b5dca000cd141ec74bf93
fabris16 <fabrisdasilvagustavo@gmail.com>
2026-06-24 12:23:52 -0300

    nanda
    nanda

---
+++ script.js
@@ -0,0 +1,5 @@
+function mensagem (){
+   let saudação= prompt (`oiie, qual é o seu nome?`)
+   let nota= prompt (`Seja bem vindo ${saudação}, qual nota você da para o filme?`)
+   let fim= prompt (`Obrigado pelo seu feedback S2`)
+}
