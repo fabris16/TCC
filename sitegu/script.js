4fee3942e8e4acc89d37bae30b98d58e5c339aed
fabris16 <fabrisdasilvagustavo@gmail.com>
2026-06-25 11:25:26 -0300

    comit
    comi

--- sitegu/script.js
+++ sitegu/script.js
@@ -1,5 +1,5 @@
 function mensagem (){
    let saudação= prompt (`oiie, qual é o seu nome?`)
-   let nota= prompt (`Seja bem vindo ${saudação}, qual nota você da para o filme?`)
+   let nota= prompt (`Seja bem vindo ${saudação}, qual nota você da para esse site?`)
    let fim= prompt (`Obrigado pelo seu feedback S2`)
 }
