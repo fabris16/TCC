a691343ec7a9e240d2ac555e1421ac85c76f8a45
fabris16 <207875017+fabris16@users.noreply.github.com>
2026-07-07 08:49:04 -0300

    cijhghgku
    zd

--- sitegu/script.js
+++ sitegu/script.js
@@ -1,5 +1,5 @@
 function mensagem (){
-   let saudação= prompt (`oiie, qual é o seu nome?`)
-   let nota= prompt (`Seja bem vindo ${saudação}, qual nota você da para esse site?`)
-   let fim= prompt (`Obrigado pelo seu feedback S2`)
+   let saudação= prompt (`oiie , qual é o seu nome?`)
+   let nota= prompt (`Você é tão boaa ${saudação}, qual nota você da para Ana?`)
+   let fim= prompt (`Obrigado pelo seu feedback S2, Ana falou que você é tão boaa`)
 }
